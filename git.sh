0eaa260fbc243bf4bedcf8594c5c6bf39f36f532
Jakob <jakob.l.weiss@gmx.de>
2019-12-07 12:31:13 +0100

    [sc]: I used the script to commit the script (and change it)

--- git.sh
+++ git.sh
@@ -1,9 +1,9 @@
-echo adding new files...
+echo "adding new files..."
 git add *
-echo staging changes...
+echo "staging changes..."
 git stage *
-echo commit changes...
+echo "commit changes..."
 git commit -m "[sc]: $1"
-echo pushing to given branch...
+echo "pushing to given branch..."
 git push origin $2
-echo script done.
+echo "script done."
